ce542a7ecc89940b2dd701f607a16ff075b28669
majorlongval <jordan.longval@outlook.com>
2026-03-17 21:09:27 -0400

    Add brain/reviewer.py

---
+++ brain/reviewer.py
@@ -0,0 +1,13 @@
+"""
+Reviewer module for automated PR reviews.
+"""
+
+class Reviewer:
+    def __init__(self):
+        pass
+
+    def review_pr(self, pr_number: int):
+        """
+        Reviews a pull request.
+        """
+        pass
